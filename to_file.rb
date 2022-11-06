572a04e6638258c4b6c37872a626f959e4cf1057
fpetrakov <jakephin2015@gmail.com>
2022-11-06 19:45:46 +0300

    ToFile module

---
+++ to_file.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module ToFile
+  def filename
+    "object_#{object_id}.txt"
+  end
+
+  def to_f
+    File.open(filename, 'w') { |f| f.write(to_s) }
+  end
+end
+
+class Person
+  include ToFile
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    name
+  end
+end
+
+Person.new('fedya').to_f
