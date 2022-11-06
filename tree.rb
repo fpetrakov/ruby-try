31d31a0a145b0c8b5734796e3bb618afe00d3936
fpetrakov <jakephin2015@gmail.com>
2022-11-06 14:30:24 +0300

    Tree class example

---
+++ tree.rb
@@ -0,0 +1,30 @@
+# !/usr/bin/ruby
+# frozen_string_literal: true
+
+class Tree
+  attr_accessor :children, :node_name
+
+  def initialize(name, children = [])
+    @children = children
+    @node_name = name
+  end
+
+  def visit_all(&block)
+    visit(&block)
+    @children.each { |c| c.visit_all(&block) }
+  end
+
+  def visit(&block)
+    block.call self
+  end
+end
+
+ruby_tree = Tree.new('Ruby', [Tree.new('Reia'), Tree.new('Adam', [Tree.new("Adam's son")])])
+
+puts 'Visiting a node'
+ruby_tree.visit { |node| puts node.node_name }
+puts
+
+puts 'Visiting entire tree'
+ruby_tree.visit_all { |node| puts node.node_name }
+puts
