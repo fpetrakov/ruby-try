398de9895b5741971daad5f452b8cd96aa4cc135
fpetrakov <jakephin2015@gmail.com>
2022-11-06 14:00:43 +0300

    change stuff for rubocop

--- guess_number.rb
+++ guess_number.rb
@@ -1,15 +1,17 @@
-#!/usr/bin/ruby
+# !/usr/bin/ruby
+# frozen_string_literal: true
+
 print "Hi, guess the number between 1 and 10 inclusively\n"
 
 random_number = rand(1..10)
 
-while true
-    guess = gets
-    if guess.to_i == random_number
-        print "You're right!\n"
-        break
-    else
-        print "You're wrong!\n"
-        next
-    end
+loop do
+  guess = gets
+  if guess.to_i == random_number
+    print "You're right!\n"
+    break
+  else
+    print "You're wrong!\n"
+    next
+  end
 end
--- named_parameter.rb
+++ named_parameter.rb
@@ -1,11 +1,13 @@
-def tell_the_truph(options={})
-	if options[:profession] == :lawyer
-		"it could be believed that this is almost certainly not false.\n"
-	else
-		"true\n"
-	end
+# frozen_string_literal: true
+
+def tell_the_truph(options = {})
+  if options[:profession] == :lawyer
+    "it could be believed that this is almost certainly not false.\n"
+  else
+    "true\n"
+  end
 end
 
 print tell_the_truph
 
-print tell_the_truph :profession => :lawyer
+print tell_the_truph profession: :lawyer
